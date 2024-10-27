2d96674f9171f56041d53f49b521ed98d6942895
prajwalk-1 <kanadeprajwal24@gmail.com>
2024-10-27 22:10:56 +0530

    updated

---
+++ header_toggle.js
@@ -0,0 +1,8 @@
+// JavaScript to Toggle Hamburger Menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
