9a3950b72bad075e1f53098491bcf74152a08a70
Prajwal Kanade <142607979+prajwalk-1@users.noreply.github.com>
2024-11-17 21:58:17 +0530

    Delete header_toggle.js

--- header_toggle.js
+++
@@ -1,8 +0,0 @@
-// JavaScript to Toggle Hamburger Menu
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
-});
